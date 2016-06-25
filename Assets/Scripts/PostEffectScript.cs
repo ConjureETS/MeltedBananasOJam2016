a6ef2b8f9e17785a2509f30d10a7bb953426e334
antoine.mcnabb <antoine.mc-nabb-baltar.1@ens.etsmtl.ca>
2016-06-25 11:52:35 -0400

    View and movement merge + stay a while mechanic first pass

---
+++ Assets/Scripts/PostEffectScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+[ExecuteInEditMode]
+public class PostEffectScript : MonoBehaviour {
+    public Material mat;
+    public float blindness = 0.5f;
+    public Rigidbody character;
+    public float blindnessRatio = 0.1f;
+    public float velocity;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (character.velocity.magnitude > 0)
+        {
+            blindness = character.velocity.normalized.magnitude * blindnessRatio;
+            blindnessRatio += 0.01f;
+        }
+        else if (blindnessRatio >= 0.1f)
+        {
+            blindness = character.velocity.normalized.magnitude * blindnessRatio;
+            blindnessRatio = 0.1f;
+        }
+       
+
+        mat.SetFloat("_Blindness", blindness);
+        Graphics.Blit(src, dest,mat);
+    }
+}
