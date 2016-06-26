2c707093059e205c76b9627cc3bc23cf46bc1760
karakune <hugopelletier783@gmail.com>
2016-06-26 10:36:34 -0400

    ajout seeker script + seeker particle effect

---
+++ Assets/Scripts/SeekerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeekerController : MonoBehaviour {
+
+    bool isAggroed;
+    float step;
+    public float speed;
+    GameObject player;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.tag == "Aggro")
+        {
+            //isAggroed = true;
+            agent.destination = player.transform.position;
+        }
+        //if (isAggroed == true)
+        //{
+        //    agent.destination = player.transform.position;
+
+        //}
+    }
+}
